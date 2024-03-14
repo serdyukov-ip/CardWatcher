efa28c19c5887d96f1e8f80b075855c5c1f310d9
serdyukov-ip <serdyukov-ip@yandex.ru>
2024-03-14 16:54:53 +0300

    Initial commit

---
+++ src/main/java/ru/serdyukov/ilya/CardWatcher/CardWatcherApplication.java
@@ -0,0 +1,13 @@
+package ru.serdyukov.ilya.CardWatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardWatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardWatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/serdyukov/ilya/CardWatcher/CardWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serdyukov.ilya.CardWatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardWatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
