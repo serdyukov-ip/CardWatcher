0cb0f3a9cefb0fbec1bdadd97e576b98fc0d6a4f
serdyukov-ip <serdyukov-ip@yandex.ru>
2024-03-22 20:31:00 +0300

    Bcrypt password encryption is configured

--- src/main/java/ru/serdyukov/ilya/CardWatcher/services/RegistrationService.java
+++ src/main/java/ru/serdyukov/ilya/CardWatcher/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.serdyukov.ilya.CardWatcher.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.serdyukov.ilya.CardWatcher.models.User;
@@ -10,14 +11,18 @@ import ru.serdyukov.ilya.CardWatcher.repositories.UsersRepository;
 public class RegistrationService {
 
     private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(UsersRepository usersRepository) {
+    public RegistrationService(UsersRepository usersRepository, PasswordEncoder passwordEncoder) {
         this.usersRepository = usersRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         usersRepository.save(user);
     }
 }
